d5be16516a64c3e2bc432a2401bc4b4285fedf44
raromale <movilrromero@gmail.com>
2022-10-12 12:48:52 +0200

    nombres iniciado

--- src/nombres.py
+++ src/nombres.py
@@ -1,1 +1,9 @@
+# -*- coding: utf-8 -*-
+# indica que la codificación es utf-8. no es necesario desde Python 3.0
 # Modulo de funciones del proyecto nombres
+
+def lee_nombres(nombre_fichero) :
+    lista = []
+
+
+    return lista
---
+++ src/nombres_test.py
@@ -0,0 +1,10 @@
+from nombres import *
+
+def main():
+    datos = lee_nombres('./data/frecuencia_nombres.csv')
+    print(datos[:3])
+    
+       
+if __name__== "__main__" : 
+    main()
+    
